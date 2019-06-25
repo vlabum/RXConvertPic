6deae4a476c8c61b4a7db6fe9b69948b6e1d5705
vlabu <vlabum@gmail.com>
2019-06-25 21:54:13 +0500

    init

---
+++ app/src/main/java/ru/vlabum/android/gb/rxconvertpic/App.kt
@@ -0,0 +1,10 @@
+package ru.vlabum.android.gb.rxconvertpic
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/ru/vlabum/android/gb/rxconvertpic/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.vlabum.android.gb.rxconvertpic.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.vlabum.android.gb.rxconvertpic.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
